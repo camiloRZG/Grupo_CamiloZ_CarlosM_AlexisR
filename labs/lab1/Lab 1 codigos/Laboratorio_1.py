bd236cc2ae396c75d88af876d1ae3744ac6cd978
Carlos <mendozacarlos1416@gmail.com>
2026-03-17 15:39:52 -0300

    Agregue los codigos que hice del lab 1

---
+++ labs/lab1/Lab 1 codigos/Laboratorio_1.py
@@ -0,0 +1,78 @@
+
+## Ejercicio 1 Ley de Ohm y Alerta de Potencia
+#ingresar el valor de voltaje y corriente en este caso usare enteros
+voltaje = int(input("ingresar voltaje = "))
+corriente = int(input("ingresar corriente = "))
+
+#Defino y calculo las variables 
+resistencia = voltaje/corriente
+potencia = voltaje*corriente
+
+#Print de variables
+print("resistencia:",resistencia)
+print("potencia:",potencia)
+
+#Condicionamiento con un comando if si es mayor a 100 print el mensaje y si es menor o igual a 1000 el otro print
+if potencia>1000:
+    print("¡Peligro!, Alta disipación de potencia detectada.")
+elif potencia <= 1000:
+    print("Operacion en rangos seguros")
+
+##Ejercicio 2
+#Ingresar valor de voltaje y voltaje minimo de operacion como pide horas enteras usare int para todo
+voltaje = int(input("ingresar voltaje = "))
+voltaje_min = int(input("ingresar voltaje minimo de operacion = "))
+
+#inicio un contador para las horas
+n = 0
+#un ciclo while mientras el voltaje sea mayor al operacional siga ejecutando
+while voltaje>=voltaje_min:
+        n += 1
+        voltaje = voltaje*0.97
+
+print("horas enteras:", n)
+
+
+##Ejercicio 3
+
+#inicio un ciclo while true para mostrar indefinidamente el menu
+while True:
+#muestro primero las opciones y luego hago el input para seleccionar la opcion
+#realizo print de cada una de las opciones disponibles
+    print("1. Convertir miliamperios (mA) a amperios (A).")
+    print("2. Convertir microfaradios (µF) a faradios (F).")
+    print("3. Convertir kiloohmios (kΩ) a ohmios (Ω).")
+    print("4. Salir.")
+
+#solicito al usuario seleccionar la variable
+    variable = int(input("Seleccionar Opción = "))
+
+#inicio los condicionales para cada variable que sea seleccionada y una opcion final por si la opcion seleccionada no es valida, en este caso usare floating porque los valores
+#ingresados pueden ser decimales 
+#si la variable seleccionada es el 1 calcular los ma
+    if variable == 1:
+        miliamperios = float(input("Ingresar Miliamperios = "))
+        print(f"amperios = {miliamperios/1000}")
+#si la variable seleccionada es 2 calcular mf
+    elif variable == 2:
+        microfaradios = float(input("Ingresar Microfaradios = "))
+        print(f"faradios = {microfaradios/1000000}")
+#si la variable seleccionada es 3 calcular ko
+    elif variable ==3:
+        kiloohmios =  float(input("Ingresar Kiloohmios = "))
+        print(f"ohmios = {kiloohmios*1000}")
+#si la variable seleccionada es 4 salir del programa
+    elif variable == 4:
+        print("Se finalizó el programa")
+        break
+#si la variable es cualquier otro numero saldremos con un invalido
+    else: print("Seleccion invalida")
+
+         
+
+
+
+
+
+
+
---
+++ labs/lab1/Lab 1 codigos/Laboratorio_1_Ej4.py
@@ -0,0 +1,19 @@
+##implemento el while true que significa mientras sea verdad
+while True: 
+        #primero realizo el input para que el usuario ingrese los grados en Celsius
+        grados = float(input("Ingresar grados Celsius: "))
+
+        #creo una condición por si el valor ingresado es menor a 20 sea invalido ya que no se especifica
+        if grados < 20:
+            print("valor invalido")
+        #creo otra condicion si grados esta entre los dos valores solicitados va a mostrar un "estado normal"
+        elif 20 <= grados <= 45:
+            print("estado normal")
+        #condicion para los rangos entre 45 y 75 grados muestra "advertencia, encendiendo ventiladores auxiliares"
+        elif 45 < grados <= 75:
+            print("advertencia, encendiendo ventiladores auxiliares")
+        #condicion para cuando los grados son mayores de 75 muestra "¡PeligroCrítico!Apagando servidor de emergencia", a su vez como se solicito
+        #se crea un break para finalizar el while true sino la maquina seguiria pidiendo un input
+        elif grados > 75:
+            print("¡PeligroCrítico!Apagando servidor de emergencia")
+            break 
